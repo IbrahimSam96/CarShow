4a44d6f0ab6d0190aa67833746bd769a683dc41b
IbrahimSam96 <aymansam68@gmail.com>
2023-01-21 19:20:02 +0300

    Removed Unwanted Space

--- pages/Components/Car.js
+++ pages/Components/Car.js
@@ -362,7 +362,6 @@ const CarScene = () => {
                                 <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
                                 <Detailed distances={[0, 10, 20]}>
                                     {displayModel()}
-
                                 </Detailed>
                             </React.Fragment>
                         )}
